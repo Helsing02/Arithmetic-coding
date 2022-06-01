48ca499c557d47635fb805591c9ed0a6b25ce990
Helsing02 <ivancool2222@gmail.com>
2022-06-02 03:57:33 +0700

    Кодирует замечательно, но не очень точно

--- arithmetic_decoding.py
+++ arithmetic_decoding.py
@@ -1,6 +1,7 @@
 class Node:
-	def __init__(self, char, freq, low=0, high=0):
+	def __init__(self, char, num_in_text, freq=0, low=0, high=0):
 		self.char=char
+		self.num_in_text=num_in_text
 		self.freq=freq
 		self.low=low
 		self.high=high
@@ -9,9 +10,7 @@ class Node:
 	def __str__(self):
 		return str(self.char)+" "+str(self.freq)+' '+str(self.low)+' '+str(self.high)
 	def new_lr(self, interval):
-		print("interval ", interval, self.low, self.high)
 		interval[0], interval[1]=interval[0]+(interval[1]-interval[0])*self.low, interval[0]+(interval[1]-interval[0])*self.high 
-
 
 
 
@@ -26,31 +25,54 @@ def decode(name_code):
 		print("Указанный файл не может быть открыт")
 		input("Нажимте Enter для закрытия консоли")
 		exit()
-	num_of_sym=int(code.read(1))
-	freq=[0 for i in range(256)]
+
+	str1=''
 	sym=code.read(1)
 	sym=sym.decode("ascii")
+	while ord(sym)!=1:
+		str1+=sym
+		sym=code.read(1)
+		sym=sym.decode("ascii")
+	end_message=int(str1)
+	while ord(sym)==1:
+		sym=code.read(1)
+		sym=sym.decode("ascii")
+
+	str1=''
+	while ord(sym)!=1:
+		str1+=sym
+		sym=code.read(1)
+		sym=sym.decode("ascii")
+	num_of_sym=int(str1)
+
 	str1=''
 	node_list=[]
-	collect=0
+	len_of_mess=0
 	while ord(sym)!=2:
 		sym=code.read(1)
 		char=sym.decode("ascii")
+		# print(char)
 		sym=code.read(1)
 		sym=sym.decode("ascii")
+		str1=''
 		while ord(sym)!=1 and ord(sym)!=2:
 			str1+=sym
 			sym=code.read(1)
 			sym=sym.decode("ascii")
+		# print(str1)
+		node_list.append(Node(char, int(str1)))
+		len_of_mess+=int(str1)
+	collect=0
+	for i in node_list:
+		i.low=collect/len_of_mess
+		collect+=i.num_in_text
+		i.high=collect/len_of_mess
 
-		node_list.append(Node(char, float(str1), collect, collect+float(str1)))
-		collect+=float(str1)
-		str1=''
 	
 	sym=code.read(1)
 	integer=int.from_bytes(sym, "big")
 	num=0
-	while len(sym)!=0:
+	while True:
 		bits=0
 		for i in range(4):
 			bits=bits<<8
@@ -68,6 +90,9 @@ def decode(name_code):
 			for j in node_list:
 				if j.low<=num and j.high>num:
 					decoded.write(j.char)
+					len_of_mess-=1
+					if len_of_mess==0:
+						return
 					num=(num-j.low)/(j.high-j.low)
 					# print(num, i, num_of_sym)
 					break
--- arithmetic_encoding.py
+++ arithmetic_encoding.py
@@ -1,6 +1,8 @@
+import math
 class Node:
-	def __init__(self, char, freq, low=0, high=0):
+	def __init__(self, char, num_in_text, freq, low=0, high=0):
 		self.char=char
+		self.num_in_text=num_in_text
 		self.freq=freq
 		self.low=low
 		self.high=high
@@ -9,43 +11,20 @@ class Node:
 	def __str__(self):
 		return str(self.char)+" "+str(self.freq)+' '+str(self.low)+' '+str(self.high)
 	def new_lr(self, interval):
-		# print("interval ", interval, self.low, self.high)
 		interval[0], interval[1]=interval[0]+(interval[1]-interval[0])*self.low, interval[0]+(interval[1]-interval[0])*self.high 
-		
-		
 
 
-def check(curr, interval):
-	if interval[0]<=curr and interval[1]>curr:
-		return -1
-	if interval[0]<=curr:
-		return 0
-	return 1
+def count_entr(node_list):
+	a=node_list[len(node_list)-1].freq
+	log=-math.log(a, 10)
+	print(int(32/log))
+	return int(32/log)
 
-def write(fileadr, interval):
-	cont=0
-	bits=0
-	for i in range(1, 33):
-		bits=bits<<1
-		# print("write ", interval, cont+1/(2**i))
-		if check(cont+1/(2**i), interval)==-1:
-			bits=bits|1
-			bits=bits<<(32-i)
-			for j in range(3, -1, -1):
-				new_i=bits>>8*j
-				fileadr.write(bytes([new_i&255]))
-				# print(bytes([new_i&255]))
-			break
-		# print(cont+1/(2**i))
-		if check(cont+1/(2**i), interval)==1:
-			bits=bits|1
-			cont+=1/(2**i)
-	interval[0]=0
-	interval[1]=1
+
 
 def count_freq(name):
 	count=0
-	freq=[0 for i in range(256)]
+	freq=[0 for i in range(129)]
 	try:
 		read_file=open(name, "r")
 	except:
@@ -63,15 +42,14 @@ def count_freq(name):
 		sym=read_file.read(1)
 		count+=1
 	read_file.close()
-	for i in range(256):
-		freq[i]/=count
+	freq[128]=count
 	return freq
 
 def make_list(freq):
 	node_list=[]
-	for i in range(256):
+	for i in range(128):
 		if freq[i]!=0:
-			node_list.append(Node(chr(i), freq[i]))
+			node_list.append(Node(chr(i), freq[i], freq[i]/freq[128]))
 	node_list=sorted(node_list, key=lambda i: i.freq, reverse=True)
 	collect=0
 	for i in node_list:
@@ -80,7 +58,33 @@ def make_list(freq):
 		i.high=collect
 	return node_list
 
+def check(curr, interval):
+	if interval[0]<=curr and interval[1]>curr:
+		return -1
+	if interval[0]<=curr:
+		return 0
+	return 1
+
+def write(fileadr, interval):
+	cont=0
+	bits=0
+	for i in range(1, 33):
+		bits=bits<<1
+		if check(cont+1/(2**i), interval)==-1:
+			bits=bits|1
+			bits=bits<<(32-i)
+			for j in range(3, -1, -1):
+				new_i=bits>>8*j
+				fileadr.write(bytes([new_i&255]))
+			break
+		if check(cont+1/(2**i), interval)==1:
+			bits=bits|1
+			cont+=1/(2**i)
+	interval[0]=0
+	interval[1]=1
+
 def encoding(name, node_list):
+	# num_of_sym=count_entr(node_list)
 	num_of_sym=5
 	try:
 		to_encode=open(name, "r")
@@ -89,9 +93,11 @@ def encoding(name, node_list):
 		print("Указанный файл не может быть открыт")
 		input("Нажимте Enter для закрытия консоли")
 		exit()
+	encoded.write(('\x01'*len(str(num_of_sym))).encode("ascii"))
+	encoded.write('\x01'.encode("ascii"))
 	encoded.write(str(num_of_sym).encode("ascii"))
 	for i in node_list:
-		encoded.write(('\x01'+i.char+str(i.freq)).encode("ascii"))
+		encoded.write(('\x01'+i.char+str(i.num_in_text)).encode("ascii"))
 	encoded.write("\x02".encode("ascii"))
 
 	curr_num=0
